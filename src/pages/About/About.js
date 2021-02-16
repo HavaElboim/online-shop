0a82a88c4149606d9f6495458c037565528b3bc4
HavaElboim <h.elboim@gmail.com>
2021-02-16 16:15:48 +0200

    added information to the About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <div>
+        Welcome to the new online shop. Choose your products category to
+        display. Hurry! - the sale ends soon!
+      </div>
+    </div>
+  );
 };
 
 export default About;
