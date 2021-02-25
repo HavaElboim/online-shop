80ed46ee31600eb6aa898fc47b545dbd1bb99679
HavaElboim <h.elboim@gmail.com>
2021-02-25 10:20:26 +0200

    products page for each productid fixed ProductInfo

--- src/pages/ProductInfo/ProductInfo.js
+++ src/pages/ProductInfo/ProductInfo.js
@@ -1,165 +1,34 @@
-import { React, useEffect, useState } from "react";
+import { React, useState, useEffect } from "react";
+import "./ProductInfo.css";
 
 const ProductInfo = ({ match }) => {
-  //const ProductInfo = (props) => {
   const [productData, setData] = useState({});
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${match.params.productid}`)
-      .then((result) => result.json())
-      .then(
-        (data) => {
-          setData(data);
-          //fetched = true;
-        },
-        [match.params.productid]
-      );
-    console.log("fetched ");
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return <div>prod details: {productData.title}</div>;
-
-  //console.log("in prodinfo page");
-  //console.log("match is ", props);
-  //return <div> asdfa}</div>;
-};
-export default ProductInfo;
-
-//import { React, useEffect, useState } from "react";
-////import { React } from "react";
-
-//import ProductDisplayClass from "../../components/ProductDisplayClass/ProductDisplayClass";
-//import sale from "../../components/ProductDisplayClass/sale.png";
-
-//const ProductInfo = ({ match }) => {
-//const [productData, setData] = useState([]);
-
-//const color = "red";
-
-//const [salesProductsIds] = [1, 3, 5, 6];
-/*
-  console.log("in productinfo");
-  return <div>product</div>;
-};
-export default ProductInfo;
-  /*
-  let fetched = false;
-
-  useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${match.params.productid}`)
-      .then((result) => result.json())
-      .then((data) => {
-        setData(data);
-        fetched = true;
-      });
-    console.log("fetched ");
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (fetched) {
-    console.log("displaying");
-    return <div>ProductInfo from :{match.params.productid} </div>;
-  }
-
-  */
-
-/*const newPrice = salesProductsIds.includes(productid)
-    ? ` Sale: $ ${+(price * 0.9).toFixed(2)}`
-    : "";*/
-
-/*useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((result) => result.json())
-      .then((data) => {
-        setData(data);
-      });
-    console.log("fetched ");
-  }, []);*/
-
-/*
-
-  useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${productid}`)
       .then((response) => response.json())
       .then((data) => setData(data));
     console.log(
       "descr is ",
       productData,
-      ` from https://fakestoreapi.com/products/${productid}`
+      ` from https://fakestoreapi.com/products/${match.params.productid}`
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  });
-
-  /*
-
-    const [products, setProducts] = useState([]);
-  const [isSale, setSale] = useState("true");
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((result) => result.json())
-      .then((data) => {
-        setProducts(data);
-      });
-    console.log("fetched ");
   }, []);
 
-  useEffect(() => {
-    console.log("prods are ", products);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <div>
-      <h2>Product info</h2>
-      <div>Your Product description:</div>
-      <h1>{title}</h1>
-      <div className="product-info">
-        {newprice && issale ? <img src={sale} alt="sale item"></img> : null}
-        <h6
-          style={{
-            color: newprice && issale ? color : "black",
-          }}
-        >
-          {title}
-        </h6>
+  if (productData) {
+    return (
+      <div>
+        <h1>{productData.title}</h1>
+        <div className="product-image">
+          <img src={productData.image} alt={""} />
+        </div>
+        <div>
+          <div className="product-info">{productData.description}</div>
+          <div className="product-info">$ {productData.price}</div>
+        </div>
       </div>
-      <div className="product-image">
-        <img src={image} alt={""} />
-      </div>
-      <div className="product-info">
-        <h5>$ {price}</h5>
-        <h5
-          style={{
-            color: color,
-            display: newprice && issale ? "block" : "none",
-          }}
-        >
-          {newprice}
-        </h5>
-        <h5>{productData.description}</h5>
-      </div>
-    </div>
-  );
-  */
-
-/*
-
-// TodoDetails page for product page:
-
-import { useEffect, useState } from "react";
-
-const TodoDetails = ({ match }) => {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.cypress.io/todos/${match.params.todoId}`)
-      .then((response) => response.json())
-      .then((data) => setTodo(data));
-  }, [match.params.todoId]);
-
-  return <div>Todo Details: {todo ? todo.title : ""}</div>;
+    );
+  }
 };
-*/
+export default ProductInfo;
